5fb7a993dcf2bce8c627a091d8042c97d13863a3
madhu <mbjavaregowda@qwinixtech.com>
2014-05-22 13:51:06 +0530

    tweet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,33 @@
 
 
 class UsersController < ApplicationController
-  
+
   def new
-     # @users = User.all
-    @user = User.new
-  end
+   @user = User.new
+ end
 
-def show
-    @user = current_user
-  end
-
+ def show
+  @user = current_user
+end
 
 
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-        session[:user_id] = @user.id
+
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
     redirect_to articles_path, :notice => "Signed up!"
-    else
-      render 'new'
-    end
+  else
+    render 'new'
   end
-
-
-def twitter_login
- omniauth = request.env['omniauth.auth']   # This contains all the details of the user say Email, Name, Age so that you can store it in your application db.
-    redirect_to "welcome_index"
-  end
+end
 
 
 
-   private
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+private
+def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation)
+end
 end
 
