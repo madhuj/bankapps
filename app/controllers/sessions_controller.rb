d046f24d00a9ab41a15ad762618c1fa32f372ce9
madhu <mbjavaregowda@qwinixtech.com>
2014-05-23 16:02:38 +0530

    sign path clear

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,10 @@ class SessionsController < ApplicationController
   def create
     if params[:provider]
      auth = request.env["omniauth.auth"]
+     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+     session[:user_id] = user.id 
      flash[:alert] = "you are successfully logged in"
-     redirect_to root_url 
+     redirect_to articles_url 
    elsif
     user = User.authenticate(params[:email], params[:password]) 
     session[:user_id] = user.id  
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   attr_accessor :email, :password, :password_confirmation
   before_save :encrypt_password
   
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  # validates_confirmation_of :password
+  # validates_presence_of :password, :on => :create
+  # validates_presence_of :email
+  # validates_uniqueness_of :email
   
 
   def self.authenticate(email, password)
